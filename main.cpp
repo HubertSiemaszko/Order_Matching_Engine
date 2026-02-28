ff94c45f99d28774f76ee08437576a2e3647c372
HubertSiemaszko <hubert.siemaszko2005@gmail.com>
2026-02-28 19:02:24 +0100

    Change orderbook

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <vector>
 #include <map>
+#include <list>
 #include <functional>
 struct Order {
     unsigned long long int OrderId;
@@ -20,56 +21,58 @@ struct Level {
     std::deque<Order> orders;
 };
 
+struct OrderLocation {
+    unsigned long long int price;
+    bool isBuy;
+    std::list<Order>::iterator it;
+};
+
 class OrderBook {
     public:
     void addOrder(Order newOrder) {
         if (newOrder.isBuy) {
             bids[newOrder.Price].push_back(newOrder);
+            idToOrder[newOrder.OrderId]=&asks[newOrder.Price].back();
             std::cout<<"Dodano zlecenie Kupna:"<<newOrder.OrderId<<std::endl;
             std::cout<<"Wartosc:"<<newOrder.Price<<std::endl;
+            std::cout<<"Ilosc:"<<newOrder.Quantity<<std::endl;
         }
         else {
             asks[newOrder.Price].push_back(newOrder);
             std::cout<<"Dodano zlecenie Sprzedaży:"<<newOrder.OrderId<<std::endl;
             std::cout<<"Wartosc:"<<newOrder.Price<<std::endl;
+            std::cout<<"Ilosc:"<<newOrder.Quantity<<std::endl;
         }
         matchOrders();
     }
 
     void matchOrders(){
         if (asks.empty()||bids.empty()) {return;}
-        unsigned long long int bestPriceAsk = asks.begin()->first;
-        unsigned long long int bestPriceBid = bids.begin()->first;
-        while (bestPriceBid >= bestPriceAsk) {
-            bestPriceAsk = asks.begin()->first;
-            bestPriceBid = bids.begin()->first;
-            Order& sellOrder = asks.begin()->second.front();
-            Order& buyOrder = bids.begin()->second.front();
-            if (buyOrder.Quantity>=sellOrder.Quantity) {
-                buyOrder.Quantity -= sellOrder.Quantity;
-                asks.begin()->second.pop_front();
-                if (asks.begin()->second.empty()) {
-                    asks.erase(asks.begin());
-                }
+        while (!asks.empty() && !bids.empty() && bids.begin()->first >= asks.begin()->first) {
+            auto itAsk = asks.begin();
+            auto itBid = bids.begin();
+            Order& sellOrder = itAsk->second.front();
+            Order& buyOrder = itBid->second.front();
 
-                if (buyOrder.Quantity==0) {
-                    bids.begin()->second.pop_front();
-                    if (asks.begin()->second.empty()) {
-                        asks.erase(asks.begin());
-                    }
+            unsigned long quantityToTrade = std::min(sellOrder.Quantity, buyOrder.Quantity);
+
+            std::cout << "TRANSAKCJA: " << quantityToTrade << " sztuk po cenie " << itAsk->first << std::endl;
+
+            sellOrder.Quantity -= quantityToTrade;
+            buyOrder.Quantity -= quantityToTrade;
+
+            if (sellOrder.Quantity == 0) {
+                itAsk->second.pop_front();
+                if (itAsk->second.empty()) {
+                    asks.erase(itAsk);
                 }
-                std::cout<<"ZLECENIE WYKONANE pierwsza ZA "<<bestPriceAsk<<std::endl;
             }
-            else if (buyOrder.Quantity<sellOrder.Quantity) {
-                sellOrder.Quantity -= buyOrder.Quantity;
-                bids.begin()->second.pop_front();
-                if (asks.begin()->second.empty()) {
-                    asks.erase(asks.begin());
+
+            if (buyOrder.Quantity == 0) {
+                itBid->second.pop_front();
+                if (itBid->second.empty()) {
+                    bids.erase(itBid);
                 }
-                std::cout<<"ZLECENIE WYKONANE druga ZA "<<bestPriceAsk<<std::endl;
-            }
-            else {
-                break;
             }
 
         }
@@ -84,22 +87,38 @@ class OrderBook {
                 std::cout<<order.OrderId<<" "<<order.Price<<" "<<order.Quantity<<std::endl;
             }
         }
+        std::cout<<"BIDS"<<std::endl;
+        for (auto const& [price, queue] : bids)
+        {
+            for (auto const& order: queue) {
+                std::cout<<order.OrderId<<" "<<order.Price<<" "<<order.Quantity<<std::endl;
+            }
+        }
 
     }
 
+
+    void IDLookup(){
+        unsigned long long int IdLookup;
+        std::cin>>IdLookup;
+        auto findId=idToOrder.find(IdLookup);
+        if (findId != idToOrder.end()){
+            findId->second->
+        }
+    }
     private:
         std::map<unsigned long long int, std::deque<Order>> asks; //from smallest to biggest
         std::map<unsigned long long int, std::deque<Order>, std::greater<unsigned long long int>> bids; //from biggest to smallest
-
+        std::unordered_map<unsigned long long int, Order*> idToOrder;
 };
 
 int main() {
     Order bids[2] = {
-        Order(1, 105, 10, true),
+        Order(1, 105, 10, true), //oferty kupna
         Order(2, 110, 5, true)
     };
     Order asks[2]={
-        Order(1, 105, 10, false),
+        Order(1, 105, 10, false), //oferty sprzedazy
         Order(2, 110, 5, false)
     };
     OrderBook orderBook;
